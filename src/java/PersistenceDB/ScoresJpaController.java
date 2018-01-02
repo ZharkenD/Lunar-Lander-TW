6032aff49a0e82ebdcb63207c8e286fcc0aeb001
cdore <cdore@PortatilCarlos>
2018-01-02 21:02:21 +0100

    Added JPA Controller from the entities & Created Registration Servlet

--- src/java/PersistenceDB/ScoresJpaController.java
+++ src/java/PersistenceDB/ScoresJpaController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PersistenceDB;
 
 import PersistenceDB.exceptions.NonexistentEntityException;
@@ -15,10 +10,6 @@ import javax.persistence.EntityNotFoundException;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-/**
- *
- * @author cdore
- */
 public class ScoresJpaController implements Serializable {
 
     public ScoresJpaController(EntityManagerFactory emf) {
