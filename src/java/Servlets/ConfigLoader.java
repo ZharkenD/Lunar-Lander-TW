af092522e4d67324ead7f357d0ece5ed1d706c7c
cdore <cdore@PortatilCarlos>
2018-01-07 20:52:15 +0100

    Update method of load configurations.

--- src/java/Servlets/ConfigLoader.java
+++ src/java/Servlets/ConfigLoader.java
@@ -1,6 +1,7 @@
 package Servlets;
 
 import PersistenceDB.Configurations;
+import PersistenceDB.ConfigurationsJpaController;
 import PersistenceDB.Users;
 import PersistenceDB.UsersJpaController;
 import com.google.gson.Gson;
@@ -8,6 +9,8 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
@@ -22,26 +25,75 @@ public class ConfigLoader extends HttpServlet {
      *
      * @param request
      * @param response
+     * @throws java.io.IOException
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
-        UsersJpaController ufc = new UsersJpaController(emf);
-        String username = getUserFromCookie(request);
-        Users user = ufc.findUser(username);
-
-        if (user.getConfigurationsCollection() != null) {
-            Collection<Configurations> configList = user.getConfigurationsCollection();
-            Gson gson = new GsonBuilder().create();
-            String json = gson.toJson(configList);
-            response.setContentType("application/json");
-            PrintWriter pw;
-            try {
-                pw = response.getWriter();
-                pw.println(json);
-            } catch (IOException ex) {
-                Logger.getLogger(ConfigLoader.class.getName()).log(Level.SEVERE, null, ex);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+            UsersJpaController ufc = new UsersJpaController(emf);
+            Users user = ufc.findUser(getUserFromCookie(request));
+            
+            if (user.getConfigurationsList() != null) {
+                Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+                response.setContentType("application/json");
+                PrintWriter pw = response.getWriter();
+                pw.println(gson.toJson(user.getConfigurationsList()));
             }
+        } catch (Exception e) {
+            Map<String, String> emess = new HashMap<>();
+            emess.put("error", "Server error");
+
+            Gson gson = new GsonBuilder().create();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.println(gson.toJson(emess));
+
+        }
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws java.io.IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+            UsersJpaController ufc = new UsersJpaController(emf);
+            Users user = ufc.findUser(getUserFromCookie(request));
+            ConfigurationsJpaController cjc = new ConfigurationsJpaController(emf);
+
+            Configurations config = new Configurations();
+
+            config.setConfigureName(request.getParameter("nameConfig"));
+            config.setDiffId(new Integer(request.getParameter("difConfig")));
+            config.setSpaceshipId(new Integer(request.getParameter("shipConfig")));
+            config.setPlanetId(new Integer(request.getParameter("landConfig")));
+            config.setUserId(user);
+
+            cjc.create(config);
+
+            Map<String, String> emess = new HashMap<>();
+            emess.put("mess", "The configuration has been saved successfully");
+            Gson gson = new GsonBuilder().create();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.println(gson.toJson(emess));
+
+        } catch (Exception e) {
+            Map<String, String> emess = new HashMap<>();
+            emess.put("error", "Server error");
+
+            Gson gson = new GsonBuilder().create();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.println(gson.toJson(emess));
         }
     }
 
@@ -62,39 +114,4 @@ public class ConfigLoader extends HttpServlet {
         return username;
     }
 
-    /**
-     *
-     * @param request
-     * @param response
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
-        UsersJpaController ufc = new UsersJpaController(emf);
-        String username = getUserFromCookie(request);
-        Users user = ufc.findUser(username);
-        Configurations config = new Configurations();
-
-        config.setConfigureName(request.getParameter("nameConfig"));
-        config.setDiffId(new Integer(request.getParameter("difConfig")));
-        config.setSpaceshipId(new Integer(request.getParameter("nave")));
-        config.setPlanetId(new Integer(request.getParameter("lugar")));
-        config.setUserId(user);
-
-        if (user.getConfigurationsCollection() != null) {
-            Collection<Configurations> configList = user.getConfigurationsCollection();
-            configList.add(config);
-            user.setConfigurationsCollection(configList);
-        } else {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            response.setContentType("application/json");
-            PrintWriter pw;
-            try {
-                pw = response.getWriter();
-                pw.println("{\"error\":\"Failed to save configurations\"}");
-            } catch (IOException ex) {
-                Logger.getLogger(ConfigLoader.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
 }
